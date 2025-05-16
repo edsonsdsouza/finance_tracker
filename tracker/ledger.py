d5143d194489442ea12bc6161ffb7e21533a487e
sooraj <edsondsouzadev@gmail.com>
2025-05-16 09:47:09 +0530

    feat: adding income and expenditure to csv file

--- tracker/ledger.py
+++ tracker/ledger.py
@@ -1,10 +1,31 @@
+import csv
+import os
+
 class Ledger:
 
-    def __init__(self):
-        pass
+    def __init__(self, file_path = None):
+        if file_path:
+            self.file_path = file_path
+        else:
+            self.file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'data', 'ledger.csv')
         
-    def income(self):
-        pass
+    def income(self, amount_type, amount):
+        try:
+            with open(self.file_path, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([amount_type.capitalize(), 'N/A', amount]) ## no category for income
+        except Exception as e:
+            print(f"Failed to record income!!: {e}") 
+        else: 
+            print("Income recorded")
 
-    def expenditure(self):
-        pass
+
+    def expenditure(self, amount_type, category, amount):
+        try:
+            with open(self.file_path, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([amount_type.capitalize(), category.capitalize(), amount]) ## category includes for expenditure
+        except Exception as e: 
+            print(f"Failed to record expenditure!!: {e}")
+        else:
+            print("Expenditure recorded")
